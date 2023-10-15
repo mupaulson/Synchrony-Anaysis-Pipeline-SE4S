52cbde2e69b1e80d42efade13ec47d0d6fff6e36
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-10-15 15:32:31 -0600

    data loader for csv

---
+++ src/load_data.py
@@ -0,0 +1,10 @@
+from animal_data import AnimalData
+
+# Create an instance from the CSV
+animal_data = AnimalData.from_csv('../data/test.csv')
+
+# Get data for a specific cell
+print(animal_data.get_data_for_cell('C000'))
+
+# Get data for all cells at a specific timestamp
+print(animal_data.get_data_at_time(1650475072.560337))
