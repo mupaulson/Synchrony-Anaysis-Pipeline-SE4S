09b78ea04b1379987c0f57ff73798a4b6d3ec375
Emma Keppler <emmakeppler@cu-genvpn-comp-10.180.25.57.int.colorado.edu>
2023-10-23 18:24:51 -0600

    fixed errors

--- src/correlations.py
+++ src/correlations.py
@@ -3,21 +3,15 @@ import numpy as np
 import scipy.stats as sps
 from animal_data import AnimalData as ad
 
-# uses AnimalData class to access data from a list of cells and returns a dataframe across all timepoints
-def animal_data_to_df(data, cells):
-    df = pd.DataFrame(data=[],columns=cells)
-
-    for c in cells:
-        t = data.get_data_for_cell(c)
-        time,cell =  map(list,zip(*test))
-        df[c] = cell
-        
-    return df
 
 
 # correlations - requires 2 dataframes and uses scipy correlate to correlate all cell values in the dataframes
 # returns 2 dataframes - corr coef and p-values
+# what to do when dataframes are different sizes
 def correlation_matrix(dataframe1, dataframe2):
+    
+    data1 = dataframe1
+    data2 = dataframe2
    
     col1 = [c for c in data1.columns.values]
     col2 = [c for c in data2.columns.values]
@@ -31,7 +25,7 @@ def correlation_matrix(dataframe1, dataframe2):
         for c2 in col2:
             a1 = data1[c1].astype(float)
             a2 = data2[c2].astype(float)
-            (p,r) = sps.pearsonr(a1,a2)
+            (r,p) = sps.pearsonr(a1,a2)
             
             corr_p.loc[c1,c2] = p
             corr_r.loc[c1,c2] = r
