5b38a6a8cf51c9a8fcabd62951b09cb6856e43d8
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-10-27 14:31:58 -0600

    create unit and functional tests for load_data.py

--- tests/functional_tests/test_load_data_functional.py
+++ tests/functional_tests/test_load_data_functional.py
@@ -22,6 +22,24 @@ class TestLoadData(unittest.TestCase):
             # Ensure the difference between timestamps is within threshold
             self.assertTrue(abs(timestamp1 - timestamp2) <= THRESHOLD)
 
+    def test_adjust_timestamps_no_change(self):
+        """Test that adjust_timestamps doesn't change timestamps if difference is greater than the threshold."""
+        mock_data3 = AnimalData({"C000": [(0.0, 1), (0.2501, 2), (0.5002, 3)], "C001": [(0.0, 1), (0.2501, 2), (0.5002, 3)]})
+        mock_data4 = AnimalData({"C000": [(0.0, 1), (0.2, 2), (0.4, 3)], "C001": [(0.0, 1), (0.2, 2), (0.4, 3)]})
+        
+        adjust_timestamps(mock_data3, mock_data4)
+        
+        # Check that timestamps weren't adjusted because the difference exceeds the threshold
+        self.assertNotEqual(mock_data3.cell_data["C000"][1][0], mock_data4.cell_data["C000"][1][0])
+        
+    def test_invalid_data_handling(self):
+        """Test that the function raises an error for mismatched datasets."""
+        mock_data5 = AnimalData({"C000": [(0.0, 1), (0.2, 2)]})
+        mock_data6 = AnimalData({"C000": [(0.0, 1), (0.2, 2), (0.4, 3)]})
+
+        with self.assertRaises(ValueError):
+            adjust_timestamps(mock_data5, mock_data6)
+
     def tearDown(self):
         """Clean up after tests."""
         # If your tests create files or modify any shared resources, clean them up here.
@@ -29,24 +47,6 @@ class TestLoadData(unittest.TestCase):
             os.remove("../data/4659-test2-remap.csv")
         if os.path.exists("../data/4807-test2-remap.csv"):
             os.remove("../data/4807-test2-remap.csv")
-
-def test_adjust_timestamps_no_change(self):
-    """Test that adjust_timestamps doesn't change timestamps if difference is greater than the threshold."""
-    mock_data3 = AnimalData({"C000": [(0.0, 1), (0.2501, 2), (0.5002, 3)], "C001": [(0.0, 1), (0.2501, 2), (0.5002, 3)]})
-    mock_data4 = AnimalData({"C000": [(0.0, 1), (0.2, 2), (0.4, 3)], "C001": [(0.0, 1), (0.2, 2), (0.4, 3)]})
-    
-    adjust_timestamps(mock_data3, mock_data4)
-    
-    # Check that timestamps weren't adjusted because the difference exceeds the threshold
-    self.assertNotEqual(mock_data3.cell_data["C000"][1][0], mock_data4.cell_data["C000"][1][0])
-    
-def test_invalid_data_handling(self):
-    """Test that the function raises an error for mismatched datasets."""
-    mock_data5 = AnimalData({"C000": [(0.0, 1), (0.2, 2)]})
-    mock_data6 = AnimalData({"C000": [(0.0, 1), (0.2, 2), (0.4, 3)]})
-
-    with self.assertRaises(ValueError):
-        adjust_timestamps(mock_data5, mock_data6)
 
 if __name__ == "__main__":
     unittest.main()
