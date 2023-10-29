eee16664b433cdd13bce53fa36ac9880eacd51a9
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-10-29 16:22:12 -0600

    add flags and command line functionality

--- src/load_data.py
+++ src/load_data.py
@@ -1,17 +1,53 @@
 from animal_data import AnimalData
+import argparse
+
+def get_args():
+    """ Returns arguments passed through the command line.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    args:   argparse.Namespace
+            All of the arguments passed through the command line
+             
+    """
+    parser = argparse.ArgumentParser(description='Parse animal data',
+                                    prog='load_data.py')
+    parser.add_argument('-i',
+                        type=str,
+                        help='Input file name',
+                        required=True)
+    parser.add_argument('-r',
+                        action='store_true',
+                        help='Remap time values',
+                        required=False)
+    parser.add_argument('-o',
+                        type=str,
+                        help='Output file name',
+                        required=True)
+    args = parser.parse_args()
+
+    return args
 
 def main():
-    # Load CSVs
-    animal1_data = AnimalData.from_csv("../data/4659-test2.csv")
-    animal2_data = AnimalData.from_csv("../data/4807-test2.csv")
+    args = get_args()
 
-    # Adjust their timestamps
-    animal1_data.remap_time_values()
-    animal2_data.remap_time_values()
+    input_filename = args.i
+    remap = args.r
+    output_filename = args.o
+
+    # Load CSVs
+    animal_data = AnimalData.from_csv(str(input_filename))
+
+    # Adjust the timestamps
+    if remap:
+        animal_data.remap_time_values()
 
     # Save time adjusted datasets
-    animal1_data.save_to_csv("../data/4659-test2-remap.csv")
-    animal2_data.save_to_csv("../data/4807-test2-remap.csv")
+    animal_data.save_to_csv(str(output_filename))
 
     print("CSVs remapped and saved successfully.")
 
