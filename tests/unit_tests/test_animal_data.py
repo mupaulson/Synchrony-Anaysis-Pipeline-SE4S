3bd1bdca60dad86e134bfd254944b21a2c4e62bc
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-10-29 18:00:43 -0600

    initial unit tests

---
+++ tests/unit_tests/test_animal_data.py
@@ -0,0 +1,26 @@
+import unittest
+import sys
+
+sys.path.insert(0, '../../src')  # noqa
+from animal_data import AnimalData
+
+sys.path.insert(0, '../../src')  # noqa
+from load_data import get_args, main
+
+class TestLoadData(unittest.TestCase):
+    # === from_csv() ===
+    # Positive case for from_csv()
+    def test_read_data_from_file(self):
+        animal_data = AnimalData.from_csv('../data/4807-test2.csv')
+        self.assertEqual(animal_data.animal_num, 4807)
+        test = animal_data.cell_data["C000"][0]
+        self.assertEqual(test, (1657027819.110297, 18.97811))
+
+    # Negative case for read_data_from_file() with nonexistent file
+    def test_read_data_from_file_nonexistent(self):
+        with self.assertRaises(SystemExit) as cm:
+            AnimalData.from_csv('nonexistent.txt')
+        self.assertEqual(cm.exception.code, 3)
+
+if __name__ == '__main__':
+    unittest.main()
