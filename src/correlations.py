e6920eaa2123281f72a9e9445cec85041172049f
Emma Keppler <emmakeppler@emmas-mbp-3.mynetworksettings.com>
2023-11-26 20:38:55 -0700

    updated correlations and unit testing

--- src/correlations.py
+++ src/correlations.py
@@ -12,8 +12,8 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
-    
+
+
 # define parser for help with the get_column function
 def get_args():
     parser = argparse.ArgumentParser(
@@ -27,27 +27,28 @@ def get_args():
     return args
 
 
-# correlations - requires 2 dataframes and uses scipy correlate to correlate all cell values in the dataframes
+# correlations - requires 2 dataframes and uses scipy correlate
+# to correlate all cell values in the dataframes
 # returns 2 dataframes - corr coef and p-values
-# what to do when dataframes are different sizes  - if different sizes need to 
+# what to do when dataframes are different sizes  - if different sizes need to
 # cut off longest array - should only happen if timepoints not aligned
 def correlation_matrix(dataframe1, dataframe2):
-    
+
     data1 = dataframe1
     data2 = dataframe2
-   
+
     col1 = [c for c in data1.columns.values]
     col2 = [c for c in data2.columns.values]
 
-    #convert columns to numpy to correlate
-    #if all values in the column are the same NaN values will be returned
-    corr_p = pd.DataFrame(data=[],index = col1, columns = col2)
-    corr_r = pd.DataFrame(data=[],index = col1, columns = col2)
-    
-    #cut off extra data from end so dataframes can be the same length
+    # convert columns to numpy to correlate
+    # if all values in the column are the same NaN values will be returned
+    corr_p = pd.DataFrame(data=[], index=col1, columns=col2)
+    corr_r = pd.DataFrame(data=[], index=col1, columns=col2)
+
+    # cut off extra data from end so dataframes can be the same length
     len1 = len(data1)
     len2 = len(data2)
-    
+
     if len1 != len2:
         if len1 > len2:
             data1 = data1.iloc[:len2]
@@ -57,27 +58,27 @@ def correlation_matrix(dataframe1, dataframe2):
             print('dataframe 2 too long - cut off to match')
         else:
             print('error matching lengths')
-            sys.exit(1) 
-        
+            sys.exit(1)
+
     for c1 in col1:
         for c2 in col2:
             a1 = data1[c1].astype(float)
             a2 = data2[c2].astype(float)
-            
-            #drop times where 1 value is nan/missing from either array
+
+            # drop times where 1 value is nan/missing from either array
             a1 = a1[~np.isnan(a2)]
             a2 = a2[~np.isnan(a1)]
-            
+
             a1 = a1[~np.isnan(a1)]
             a2 = a2[~np.isnan(a2)]
-            
+
             try:
-                (r,p) = sps.pearsonr(a1,a2)
-            except:
+                (r, p) = sps.pearsonr(a1, a2)
+            except:  # noqa
                 print('unable to calculate correlation')
-                sys.exit(1) 
-            
+                sys.exit(1)
+
             corr_p.loc[c1, c2] = p
             corr_r.loc[c1, c2] = r
-            
+
     return corr_p, corr_r
