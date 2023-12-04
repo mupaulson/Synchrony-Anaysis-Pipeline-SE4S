e2952bb92ce5045278dc6f2b08c1769e77b21276
mupaulson <mapa2020@colorado.edu>
2023-12-04 17:57:58 +0000

    MP current status commit, will do todos later today

--- src/correlations.py
+++ src/correlations.py
@@ -1,43 +1,23 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as sps
+import argparse
 from animal_data import AnimalData as ad
 import sys
 
 
-# define main function
-def main():
-    print(__name__)
-
-
-if __name__ == '__main__':
-    main()
-
-
-# define parser for help with the get_column function
-def get_args():
-    parser = argparse.ArgumentParser(
-                                description='correlates two dataframes',
-                                prog='correlation_matrix')
-    parser.add_argument('--dataframe1',
-                        help='pass 1 dataframe')
-    parser.add_argument('--dataframe2',
-                        help='pass second dataframe')
-    parser.add_argument('--savename',
-                        help='name for output files')
-    parser.add_argument('--save',
-                        help='save output dataframes as csv, default True')
-    args = parser.parse_args()
-    return args
-
-
-# correlations - requires 2 dataframes and uses scipy correlate
-# to correlate all cell values in the dataframes
-# returns 2 dataframes - corr coef and p-values
-# what to do when dataframes are different sizes  - if different sizes need to
-# cut off longest array - should only happen if timepoints not aligned
-def correlation_matrix(dataframe1, dataframe2, savename, save=True):
-
+# Generate correlation matrix
+def correlation_matrix(dataframe1, dataframe2):
+    """
+    Takes two dataframes of cell data and correlates all cells
+    between the dataframes. Outputs two dataframes of p-values
+    and R values for every correlation. Different size dataframes
+    are cut to be the same length, and missing data is removed.
+        Args: dataframe1, dataframe2 are the two dataframes to
+        correlate. The cell names of dataframe1 will be the
+        index of the output, and the cells in dataframe2 will be
+        the column names.
+        """
     data1 = dataframe1
     data2 = dataframe2
 
@@ -85,10 +65,44 @@ def correlation_matrix(dataframe1, dataframe2, savename, save=True):
             corr_p.loc[c1, c2] = p
             corr_r.loc[c1, c2] = r
 
-        if save is True:
-            corr_p.to_csv('../output/'+savename+'_p.csv')
-            corr_r.to_csv('../output/'+savename+'_r.csv')
-        else:
-            pass
-
     return corr_p, corr_r
+
+
+# define parser for help with the get_column function
+def get_args():
+    parser = argparse.ArgumentParser(
+                                description='correlates two dataframes',
+                                prog='correlation_matrix')
+    parser.add_argument('--dataframe1',
+                        help='pass 1 dataframe')
+    parser.add_argument('--dataframe2',
+                        help='pass second dataframe')
+    parser.add_argument('--out_file_r',
+                        help='name list for output file r values')
+    parser.add_argument('--out_file_p',
+                        help='name list for output file p values')
+    args = parser.parse_args()
+    return args
+
+
+# define main function
+def main():
+    args = get_args()
+
+    df1_file = args.dataframe1
+    df2_file = args.dataframe2
+
+    df1 = pd.read_csv(df1_file)
+    df2 = pd.read_csv(df2_file)
+
+    corr_p, corr_r = correlation_matrix(df1, df2)
+
+    r_file_out = args.out_file_r
+    p_file_out = args.out_file_p
+
+    corr_r.to_csv(r_file_out)
+    corr_p.to_csv(p_file_out)
+
+
+if __name__ == '__main__':
+    main()
