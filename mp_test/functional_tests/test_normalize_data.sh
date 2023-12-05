19182dc3d3367bd8bdf87c1fc4b07ef3583304df
mupaulson <65028218+mupaulson@users.noreply.github.com>
2023-12-05 12:20:58 -0700

    Delete mp_test directory

--- mp_test/functional_tests/test_normalize_data.sh
+++
@@ -1,9 +0,0 @@
-test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
-. ssshtest
-
-run normalize_data python ../../src/normalize_data.py --file_name ../../tests/data/4807-test.csv --out_file test_norm.csv
-assert_equal $"test_norm.csv" $(ls test_norm.csv)
-assert_exit_code 0
-
-run normalize_data_bad_data python ../../src/normalize_data.py --file_name ../test_data/string_matrix.csv --out_file test_norm.csv
-assert_exit_code 1
